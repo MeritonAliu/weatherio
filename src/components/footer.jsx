d4f6d70b7946fb005e5d597c7f53fa86dabc290d
Toni <tonialiu03@gmail.com>
2023-12-02 16:09:02 +0100

    begin footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className='credits'>
+        <p>
+        Made by <a href="https://github.com/meritonaliu" target="_blank" rel="noopener noreferrer">Meriton Aliu</a>
+        </p>
+      </div>
+      <div className='links'>
+
+      </div>
+      <div className='year'>
+        2023 Ⓒ
+      </div>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import './styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/header';
+import Footer from './components/footer';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,6 +15,7 @@ root.render(
     <BrowserRouter>
     <Header />
     <App />
+    <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
