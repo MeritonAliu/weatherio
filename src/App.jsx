3f0772e1acfe84a1b805d06400eb5368ed075912
Toni <tonialiu03@gmail.com>
2023-12-02 19:08:58 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,80 @@
+import React, { useState } from "react";
+import "./styles/main.scss";
 
-import Worldview from "./pages/worldview";
-import City from "./pages/city";
-import { Routes, Route } from "react-router-dom";
+export default function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null); // New state for tracking errors
 
-function App() {
+  const handleEnterKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      fetchWeatherData();
+    }
+  };
 
+  const fetchWeatherData = async () => {
+    setError(null);
+    setWeatherData(null);
+    if (city) {
+      const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY; // Replace with your API key
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+      try {
+        const response = await fetch(apiUrl);
+        if (response.ok) {
+          const data = await response.json();
+          setWeatherData(data);
+          console.log("Weather Data:", data);
+        } else {
+          console.error("Error fetching weather data:", response.statusText);
+          setError("Error, city not found.");
+        }
+      } catch (error) {
+        setError("An error occurred while fetching weather data.");
+        console.error("Error fetching weather data:", error);
+      }
+    }
+  };
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<City />} />
-        <Route path="/wordview" element={<Worldview />} />
-      </Routes>
+    <div className="citypage">
+        <div className="search">
+          <input
+            type="text"
+            value={city}
+            onChange={handleCityChange}
+            onKeyDown={handleEnterKeyPress}
+            placeholder="Enter city name"
+          />
+          <button onClick={fetchWeatherData}>Get Weather</button>
+        </div>
+
+        {weatherData && (
+          <div className="dataCard">
+            <div>
+              <h1>Weather Data for {weatherData.name}</h1>
+              <p>Temperature: {weatherData.main.temp}</p>
+              <p>Feels Like: {weatherData.main.feels_like}</p>
+              <p>Humidity: {weatherData.main.humidity}</p>
+              <p>Wind Speed: {weatherData.wind.speed}</p>
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                alt={weatherData.weather[0].description}
+              />
+              <p>{weatherData.weather[0].description}</p>
+            </div>
+          </div>
+        )}
+        {error && (
+          <div className="errorCard">
+            <p>{error}</p>
+          </div>
+        )}
     </div>
   );
 }
-
-export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import "../styles/header.scss"
@@ -10,13 +9,6 @@ function Header() {
     <Navbar expand="sm" className="navi" >
       <Container fluid>
         <Navbar.Brand href="/">WeatherIO</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/">City</Nav.Link>
-            <Nav.Link href="/worldview">Worldview</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
