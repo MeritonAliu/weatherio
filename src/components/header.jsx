82de362be170423d31a02eb5b1e4238de2bdf7b9
Toni <tonialiu03@gmail.com>
2023-12-02 15:43:22 +0100

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import "../styles/header.scss"
+
 
 function Header() {
   return (
-    <Navbar expand="sm" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">WeatherIO</Navbar.Brand>
+    <Navbar expand="sm" className="navi" >
+      <Container fluid>
+        <Navbar.Brand href="/">WeatherIO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+            <Nav.Link href="/">City</Nav.Link>
+            <Nav.Link href="#Worldview">Worldview</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
